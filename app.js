f6cf14c31af1faabf00a5200253825350a1edcf7
unknown <syedsameerali26@gmail.com>
2023-12-19 19:24:58 +0500

    po

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(cors({
 app.use(express.json());
 app.use(router);
 // app.use(express.static('uploads'));
-app.use('uploads', express.static('uploads'));
+app.use('./uploads', express.static('uploads'));
 // app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 
